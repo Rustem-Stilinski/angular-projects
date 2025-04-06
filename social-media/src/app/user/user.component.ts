8aa86393728a18d682ea9010b32ec79c285466cf
Rustem Aisariyev <scorus0808@gmail.com>
2025-04-06 12:45:58 -0400

    adding an user component

--- social-media/src/app/user/user.component.ts
+++ social-media/src/app/user/user.component.ts
@@ -9,5 +9,5 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-selectedUser: string = DUMMY_USERS[randomIndex];
+selectedUser = DUMMY_USERS[randomIndex];
 }
